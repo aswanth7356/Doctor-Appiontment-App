99b0c1a9e73c69b7263232188d9d298439448989
aswanth7356 <aswanth1392@gmail.com>
2024-09-24 11:46:09 +0530

    base url again changed

--- src/services/base_url.js
+++ src/services/base_url.js
@@ -1,5 +1,5 @@
 // const base_url='http://localhost:3000'
-const base_url='https://doctorappointmentbackend-my3v.onrender.com'
+const base_url='https://doctorappointmentbackend-x4we.onrender.com'
 
 
 export default base_url
