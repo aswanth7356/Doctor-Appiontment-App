522f8e62951ba16b656466d262a6fce5d93b0c5a
aswanth7356 <aswanth1392@gmail.com>
2024-10-04 22:02:11 +0530

    updated baseUrl

--- src/services/base_url.js
+++ src/services/base_url.js
@@ -1,4 +1,4 @@
-const base_url='http://localhost:3000'
-// const base_url='https://doctorappointmentbackend-x4we.onrender.com'
+// const base_url='http://localhost:3000'
+const base_url='https://doctorappointmentbackend-x4we.onrender.com'
 
 export default base_url
